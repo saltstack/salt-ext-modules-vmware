9a7fa998f2420bca311d7d5d15b4dba2ae86ecdd
shaikre <shaikre@vmware.com>
2021-07-13 15:16:55 +0530

    Added create function and corresponding tests for vmc_dhcp_profile

--- src/saltext/vmware/modules/vmc_dhcp_profiles.py
+++ src/saltext/vmware/modules/vmc_dhcp_profiles.py
@@ -17,6 +17,20 @@ def __virtual__():
     return __virtualname__
 
 
+def _create_payload_for_dhcp_profile(dhcp_profile_id, profile_type, user_input):
+    """
+    This function creates the payload for dhcp profile based on the json template and user input passed
+    """
+    template_data = getattr(
+        vmc_templates,
+        "create_dhcp_" + profile_type + "_profiles",
+        vmc_templates.create_dhcp_relay_profiles,
+    )
+    data = vmc_request.create_payload_for_request(template_data, user_input)
+    data["id"] = data["display_name"] = dhcp_profile_id
+    return data
+
+
 def get(
     hostname,
     refresh_key,
@@ -261,6 +275,159 @@ def delete(
         authorization_host=authorization_host,
         description="vmc_dhcp_profiles.delete",
         responsebody_applicable=False,
+        verify_ssl=verify_ssl,
+        cert=cert,
+    )
+
+
+def create(
+    hostname,
+    refresh_key,
+    authorization_host,
+    org_id,
+    sddc_id,
+    type,
+    dhcp_profile_id,
+    verify_ssl=True,
+    cert=None,
+    server_addresses=None,
+    tags=vmc_constants.VMC_NONE,
+    lease_time=None,
+):
+    """
+    Creates DHCP profile of given type for the given SDDC
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt vm_minion vmc_dhcp_profiles.create hostname=nsxt-manager.local type=server ...
+
+    hostname
+        The host name of NSX-T manager
+
+    refresh_key
+        API Token of the user which is used to get the Access Token required for VMC operations
+
+    authorization_host
+        Hostname of the VMC cloud console
+
+    org_id
+        The Id of organization to which the SDDC belongs to
+
+    sddc_id
+        The Id of SDDC for which the DHCP profile should be added
+
+    type
+        The type of DHCP profile for which the given DHCP belongs to. Possible values: server, relay
+
+    dhcp_profile_id
+        Id of the DHCP profile to be added to SDDC
+
+    verify_ssl
+        (Optional) Option to enable/disable SSL verification. Enabled by default.
+        If set to False, the certificate validation is skipped.
+
+    cert
+        (Optional) Path to the SSL client certificate file to connect to VMC Cloud Console.
+        The certificate can be retrieved from browser.
+
+    server_addresses
+        when type is relay, this field indicates DHCP relay addresses(DHCP server IP addresses
+        for DHCP relay configuration).
+        Both IPv4 and IPv6 addresses are supported.
+
+        when type is server, this field indicates DHCP server address in CIDR format.
+        Both IPv4 and IPv6 address families are supported.
+        Prefix length should be less than or equal to 30 for IPv4 address family
+        and less than or equal to 126 for IPv6.
+        When not specified, IPv4 value is auto-assigned to 100.96.0.1/30.
+        Note: This field is optional only when type is server and
+        is mandatory when type is relay.
+
+    tags
+        (Optional) Opaque identifiers meaningful to the user.
+
+        .. code::
+
+            tags='[
+                {
+                    "tag": "<tag-key-1>"
+                    "scope": "<tag-value-1>"
+                },
+                {
+                    "tag": "<tag-key-2>"
+                    "scope": "<tag-value-2>"
+                }
+            ]'
+
+    lease_time
+        (Optional) IP address lease time in seconds. Minimum value is 60.
+        Maximum value is 4294967295. Default value is 86400
+        Note: This field is applicable only when type is server
+
+    Example values:
+
+        .. code::
+
+            dhcp-server-profiles:
+            {
+                "server_addresses": [
+                    "10.22.12.2/23"
+                ],
+                "tags": [
+                    {
+                        "tag": "tag1",
+                        "scope": "scope1"
+                    }
+                ],
+                "lease_time": 86400
+            }
+
+            dhcp-relay-profiles:
+            {
+                "server_addresses": [
+                    "10.1.1.1"
+                ],
+                "tags": [
+                    {
+                        "tag": "tag1",
+                        "scope": "scope1"
+                    }
+                ]
+            }
+    """
+
+    log.info("Creating DHCP profile %s for SDDC %s", dhcp_profile_id, sddc_id)
+
+    profile_type = vmc_constants.DHCP_CONFIGS.format(type)
+    api_url_base = vmc_request.set_base_url(hostname)
+    api_url = (
+        "{base_url}vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/{profile_type}/{profile_id}"
+    )
+    api_url = api_url.format(
+        base_url=api_url_base,
+        org_id=org_id,
+        sddc_id=sddc_id,
+        profile_type=profile_type,
+        profile_id=dhcp_profile_id,
+    )
+
+    allowed_dict = {"server_addresses": server_addresses, "tags": tags, "lease_time": lease_time}
+
+    req_data = vmc_request._filter_kwargs(
+        allowed_kwargs=allowed_dict.keys(), allow_none=["tags"], **allowed_dict
+    )
+
+    payload = _create_payload_for_dhcp_profile(dhcp_profile_id, type, req_data)
+    return vmc_request.call_api(
+        method=vmc_constants.PUT_REQUEST_METHOD,
+        url=api_url,
+        refresh_key=refresh_key,
+        authorization_host=authorization_host,
+        description="vmc_dhcp_profiles.create",
+        data=payload,
         verify_ssl=verify_ssl,
         cert=cert,
     )
--- tests/unit/modules/test_vmc_dhcp_profiles.py
+++ tests/unit/modules/test_vmc_dhcp_profiles.py
@@ -163,3 +163,64 @@ def test_delete_dhcp_profile_called_with_url():
     call_kwargs = vmc_call_api.mock_calls[0][-1]
     assert call_kwargs["url"] == expected_url
     assert call_kwargs["method"] == vmc_constants.DELETE_REQUEST_METHOD
+
+
+def test_create_dhcp_relay_profile_when_api_should_return_successfully_created_message(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"message": "DHCP relay profile created successfully"}
+    mock_vmc_request_call_api.return_value = expected_response
+    assert (
+        vmc_dhcp_profiles.create(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="relay",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_create_dhcp_server_profile_when_api_should_return_successfully_created_message(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"message": "DHCP server profile created successfully"}
+    mock_vmc_request_call_api.return_value = expected_response
+    assert (
+        vmc_dhcp_profiles.create(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="server",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_create_dhcp_profile_called_with_url():
+    expected_url = (
+        "https://hostname/vmc/reverse-proxy/api/orgs/org_id/sddcs/sddc_id/policy/api/"
+        "v1/infra/dhcp-server-configs/dhcp_profile_id"
+    )
+    with patch("saltext.vmware.utils.vmc_request.call_api", autospec=True) as vmc_call_api:
+        vmc_dhcp_profiles.create(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="server",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+    call_kwargs = vmc_call_api.mock_calls[0][-1]
+    assert call_kwargs["url"] == expected_url
+    assert call_kwargs["method"] == vmc_constants.PUT_REQUEST_METHOD
