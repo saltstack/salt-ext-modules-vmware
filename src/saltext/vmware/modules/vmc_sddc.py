c2b8dd3faea245ce9d7549ea3fd7ab64915d8fcc
jain-prerna <83270458+jain-prerna@users.noreply.github.com>
2021-07-14 18:56:51 +0530

    Added get_by_id for vmc_sddc (#37)
    * Added get_by_id for vmc_sddc
    
    * fixed doc fromat

--- src/saltext/vmware/modules/vmc_sddc.py
+++ src/saltext/vmware/modules/vmc_sddc.py
@@ -79,3 +79,55 @@ def get(
         verify_ssl=verify_ssl,
         cert=cert,
     )
+
+
+def get_by_id(
+    hostname, refresh_key, authorization_host, org_id, sddc_id, verify_ssl=True, cert=None
+):
+    """
+    Returns a SDDC detail for the given SDDC Id
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt minion-key-id vmc_sddc.get hostname=vmc.vmware.com sddc_id=sddc_id...
+
+    hostname
+        The host name of VMC
+
+    refresh_key
+        API Token of the user which is used to get the Access Token required for VMC operations
+
+    authorization_host
+        Hostname of the Cloud Services Platform (CSP)
+
+    org_id
+        The Id of organization to which the SDDC belongs to
+
+    sddc_id
+        sddc_id of the SDDC which will be retrieved
+
+    verify_ssl
+        (Optional) Option to enable/disable SSL verification. Enabled by default.
+         If set to False, the certificate validation is skipped.
+
+    cert
+        (Optional) Path to the SSL client certificate file to connect to VMC Cloud Console.
+        The certificate can be retrieved from browser.
+
+    """
+    log.info("Retrieving the SDDC details for the sddc %s in the organization %s", sddc_id, org_id)
+    api_base_url = vmc_request.set_base_url(hostname)
+    api_url = "{base_url}vmc/api/orgs/{org_id}/sddcs/{sddc_id}".format(
+        base_url=api_base_url, org_id=org_id, sddc_id=sddc_id
+    )
+    return vmc_request.call_api(
+        method=vmc_constants.GET_REQUEST_METHOD,
+        url=api_url,
+        refresh_key=refresh_key,
+        authorization_host=authorization_host,
+        description=__virtualname__ + "." + sys._getframe().f_code.co_name,
+        verify_ssl=verify_ssl,
+        cert=cert,
+    )
