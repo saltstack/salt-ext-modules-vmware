31f78394811da77fbf186f5d6d4da8034ac1e7d0
Prerna <prjain@vmware.com>
2021-07-14 21:15:27 +0530

    Added Sddc create function

--- tests/unit/modules/test_vmc_sddc.py
+++ tests/unit/modules/test_vmc_sddc.py
@@ -138,3 +138,44 @@ def test_get_sddc_by_id_called_with_url():
     call_kwargs = vmc_call_api.mock_calls[0][-1]
     assert call_kwargs["url"] == expected_url
     assert call_kwargs["method"] == vmc_constants.GET_REQUEST_METHOD
+
+
+def test_create_sddc_when_api_should_return_api_response(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"message": "SDDC created successfully"}
+    mock_vmc_request_call_api.return_value = expected_response
+
+    assert (
+        vmc_sddc.create(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_name="test-sddc",
+            num_hosts=1,
+            provider="provider",
+            region="region",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_create_sddc_called_with_url():
+    expected_url = "https://hostname/vmc/api/orgs/org_id/sddcs"
+    with patch("saltext.vmware.utils.vmc_request.call_api", autospec=True) as vmc_call_api:
+        vmc_sddc.create(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_name="test-sddc",
+            num_hosts=1,
+            provider="provider",
+            region="region",
+            verify_ssl=False,
+        )
+    call_kwargs = vmc_call_api.mock_calls[0][-1]
+    assert call_kwargs["url"] == expected_url
+    assert call_kwargs["method"] == vmc_constants.POST_REQUEST_METHOD
