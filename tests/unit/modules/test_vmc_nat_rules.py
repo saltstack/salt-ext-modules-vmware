488a2df77c4594874e4345af04c5676ec5276e97
Naval Patel <35801101+navalkp@users.noreply.github.com>
2021-07-14 19:21:49 +0530

    Added delete method for vmc nat rules (#40)

--- tests/unit/modules/test_vmc_nat_rules.py
+++ tests/unit/modules/test_vmc_nat_rules.py
@@ -120,3 +120,46 @@ def test_get_nat_rules_by_id_called_with_url():
     call_kwargs = vmc_call_api.mock_calls[0][-1]
     assert call_kwargs["url"] == expected_url
     assert call_kwargs["method"] == vmc_constants.GET_REQUEST_METHOD
+
+
+def test_delete_nat_rule_when_api_should_return_successfully_deleted_message(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"message": "Nat rule deleted successfully"}
+    mock_vmc_request_call_api.return_value = expected_response
+    assert (
+        vmc_nat_rules.delete(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            tier1="tier1",
+            nat="nat",
+            nat_rule="nat_rule",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_delete_nat_rules_called_with_url():
+    expected_url = (
+        "https://hostname/vmc/reverse-proxy/api/orgs/org_id/sddcs/sddc_id/policy/api/"
+        "v1/infra/tier-1s/tier1/nat/nat/nat-rules/nat_rule"
+    )
+    with patch("saltext.vmware.utils.vmc_request.call_api", autospec=True) as vmc_call_api:
+        result = vmc_nat_rules.delete(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            tier1="tier1",
+            nat="nat",
+            nat_rule="nat_rule",
+            verify_ssl=False,
+        )
+    call_kwargs = vmc_call_api.mock_calls[0][-1]
+    assert call_kwargs["url"] == expected_url
+    assert call_kwargs["method"] == vmc_constants.DELETE_REQUEST_METHOD
