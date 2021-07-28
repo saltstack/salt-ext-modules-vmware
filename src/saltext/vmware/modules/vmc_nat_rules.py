899b25ecdfc494beeb9dc21e3d34b423e7016bcf
Naval Patel <35801101+navalkp@users.noreply.github.com>
2021-07-29 00:40:13 +0530

    Added create method for vmc nat rules and corresponding tests (#43)
    * Added create method for vmc nat rules and corresponding tests
    
    * Added reference to external link for Nat Rule

--- src/saltext/vmware/modules/vmc_nat_rules.py
+++ src/saltext/vmware/modules/vmc_nat_rules.py
@@ -1,6 +1,6 @@
 """
-Salt execution module for Nat Rules
-Provides methods to Create, Update, Read and Delete Nat rules.
+Salt execution module for nat rules
+Provides methods to Create, Update, Read and Delete nat rules.
 """
 import logging
 import os
@@ -16,6 +16,15 @@ __virtualname__ = "vmc_nat_rules"
 
 def __virtual__():
     return __virtualname__
+
+
+def _create_payload_for_nat_rule(rule_id, user_input):
+    """
+    This function creates the payload based on the template and user input passed
+    """
+    data = vmc_request.create_payload_for_request(vmc_templates.create_nat_rules, user_input)
+    data["id"] = data["display_name"] = rule_id
+    return data
 
 
 def get(
@@ -86,7 +95,7 @@ def get(
 
     """
 
-    log.info("Retrieving Nat rules for SDDC %s", sddc_id)
+    log.info("Retrieving nat rules for SDDC %s", sddc_id)
 
     api_url_base = vmc_request.set_base_url(hostname)
     api_url = (
@@ -170,7 +179,7 @@ def get_by_id(
         (Optional) Path to the SSL certificate file to connect to NSX-T manager.
         The certificate can be retrieved from browser.
     """
-    log.info("Retrieving Nat rule %s for SDDC %s", nat_rule, sddc_id)
+    log.info("Retrieving nat rule %s for SDDC %s", nat_rule, sddc_id)
     api_url_base = vmc_request.set_base_url(hostname)
     api_url = (
         "{base_url}vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
@@ -250,7 +259,7 @@ def delete(
         The certificate can be retrieved from browser.
 
     """
-    log.info("Deleting Nat rule %s for SDDC %s", nat_rule, sddc_id)
+    log.info("Deleting nat rule %s for SDDC %s", nat_rule, sddc_id)
     api_url_base = vmc_request.set_base_url(hostname)
     api_url = (
         "{base_url}vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
@@ -271,6 +280,243 @@ def delete(
         authorization_host=authorization_host,
         description="vmc_nat_rule.delete",
         responsebody_applicable=False,
+        verify_ssl=verify_ssl,
+        cert=cert,
+    )
+
+
+def create(
+    hostname,
+    refresh_key,
+    authorization_host,
+    org_id,
+    sddc_id,
+    tier1,
+    nat,
+    nat_rule,
+    verify_ssl=True,
+    cert=None,
+    action=None,
+    destination_network=None,
+    source_network=None,
+    translated_network=None,
+    translated_ports=vmc_constants.VMC_NONE,
+    scope=None,
+    service=None,
+    enabled=None,
+    firewall_match=None,
+    logging=None,
+    description=None,
+    tags=vmc_constants.VMC_NONE,
+    sequence_number=None,
+):
+    """
+    Create nat rules for Given SDDC
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt vm_minion vmc_nat_rules.create hostname=nsxt-manager.local tier1=cgw ...
+
+    hostname
+        The host name of NSX-T manager
+
+    refresh_key
+        refresh_key to get access token
+
+    authorization_host
+        hostname to get access token
+
+    org_id
+        org_id of the SDDC
+
+    sddc_id
+        sddc_id for which nat rules should be created
+
+    tier1
+        tier1 option are cgw and user defined tier1
+
+    nat
+        nat option are USER/default/Internal
+
+    nat_rule
+        id of specific nat rule
+
+    verify_ssl
+        (Optional) Option to enable/disable SSL verification. Enabled by default.
+        If set to False, the certificate validation is skipped.
+
+    cert
+        (Optional) Path to the SSL certificate file to connect to NSX-T manager.
+        The certificate can be retrieved from browser.
+
+    action
+        specify type of nat rule it can have value REFLEXIVE, DNAT
+
+            REFLEXIVE nat rule require
+                source_network
+                translated_network
+                service should be empty
+                translated_ports  should be None
+                destination_network should be none
+
+            DNAT  Rule require
+                destination_network
+                translated_network
+                translated_ports can be none
+                service can be none
+                source_network can be None or input network.
+
+    destination_network
+        Represents the destination network
+            This supports single IP address or comma separated list of single IP
+            addresses or CIDR. This does not support IP range or IP sets.
+
+    source_network
+        Represents the source network address
+            This supports single IP address or comma separated list of single IP
+            addresses or CIDR. This does not support IP range or IP sets.
+
+    translated_network
+        Represents the translated network address
+
+            This supports single IP address or comma separated list of single IP
+            addresses or CIDR. This does not support IP range or IP sets.
+
+    translated_ports
+        Port number or port range
+
+            Please note, if there is service configured in this nat rule, the translated_port
+            will be realized on NSX Manager as the destination_port. If there is no sevice configured,
+            the port will be ignored.
+
+    scope
+        (Optional) Array of policy paths of labels, ProviderInterface, NetworkInterface
+        If this value is not passed, then ["/infra/labels/cgw-public"] will be used by default.
+
+    service
+        (Optional) Represents the service on which the nat rule will be applied
+        If this value is not passed, then empty string will be used by default.
+
+    enabled
+        (Optional) Policy nat rule enabled flag
+
+            The flag, which suggests whether the nat rule is enabled or
+            disabled. The default is True.
+
+    firewall_match
+        (Optional) Represents the firewall match flag
+
+            It indicates how the firewall matches the address after nating if firewall
+            stage is not skipped.
+            possible values: MATCH_EXTERNAL_ADDRESS, MATCH_INTERNAL_ADDRESS
+            Default: "MATCH_INTERNAL_ADDRESS"
+
+    logging
+        (Optional) Policy nat rule logging flag
+            default: False
+
+    description
+        (Optional) Description of nat rule
+
+    tags
+        (Optional) Opaque identifiers meaningful to the API user. Maximum 30 tags can be associated:
+
+        .. code-block::
+
+            tags='[
+                {
+                    "tag": "<tag-key-1>"
+                    "scope": "<tag-value-1>"
+                },
+                {
+                    "tag": "<tag-key-2>"
+                    "scope": "<tag-value-2>"
+                }
+            ]'
+
+
+    sequence_number
+        (Optional) Sequence number of the nat rule
+            The sequence_number decides the rule_priority of a nat rule.
+            default: 0
+            type: int
+
+    Example values:
+
+        .. code-block::
+
+            {
+                "action": "REFLEXIVE",
+                "translated_network": "10.182.171.36",
+                "translated_ports": null,
+                "destination_network": "",
+                "source_network": "192.168.1.23",
+                "sequence_number": 0,
+                "service": "",
+                "logging": false,
+                "enabled": false,
+                "scope": [
+                    "/infra/labels/cgw-public"
+                ],
+                "tags": [
+                    {
+                        "tag": "tag1",
+                        "scope": "scope1"
+                    }
+                ],
+                "description": "",
+                "firewall_match": "MATCH_INTERNAL_ADDRESS"
+            }
+
+    For More info refer to this
+    `Nat Rule <https://developer.vmware.com/docs/nsx-vmc-policy/latest/data-structures/SyntheticClass/PolicyNatRule/allOf1/>`_
+
+    """
+    log.info("Creating nat rule %s for SDDC %s ", nat_rule, sddc_id)
+    api_url_base = vmc_request.set_base_url(hostname)
+    api_url = (
+        "{base_url}vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/tier-1s/{tier1}/nat/{nat}/nat-rules/{nat_rule}"
+    )
+    api_url = api_url.format(
+        base_url=api_url_base,
+        org_id=org_id,
+        sddc_id=sddc_id,
+        tier1=tier1,
+        nat=nat,
+        nat_rule=nat_rule,
+    )
+
+    allowed_dict = {
+        "action": action,
+        "description": description,
+        "destination_network": destination_network,
+        "scope": scope,
+        "service": service,
+        "source_network": source_network,
+        "tags": tags,
+        "translated_network": translated_network,
+        "translated_ports": translated_ports,
+        "enabled": enabled,
+        "firewall_match": firewall_match,
+        "logging": logging,
+        "sequence_number": sequence_number,
+    }
+    req_data = vmc_request._filter_kwargs(
+        allowed_kwargs=allowed_dict.keys(), allow_none=["translated_ports", "tags"], **allowed_dict
+    )
+
+    data = _create_payload_for_nat_rule(nat_rule, req_data)
+
+    return vmc_request.call_api(
+        method=vmc_constants.PUT_REQUEST_METHOD,
+        url=api_url,
+        refresh_key=refresh_key,
+        authorization_host=authorization_host,
+        description="vmc_nat_rule.create",
+        data=data,
         verify_ssl=verify_ssl,
         cert=cert,
     )
