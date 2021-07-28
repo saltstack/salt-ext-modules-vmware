48ad93dba4f93d653843634c98cc2279add0287f
shaikres <83271164+shaikres@users.noreply.github.com>
2021-07-29 00:58:46 +0530

    Added update function and corresponding tests for vmc_dhcp module (#41)

--- tests/unit/modules/test_vmc_dhcp_profiles.py
+++ tests/unit/modules/test_vmc_dhcp_profiles.py
@@ -165,7 +165,7 @@ def test_delete_dhcp_profile_called_with_url():
     assert call_kwargs["method"] == vmc_constants.DELETE_REQUEST_METHOD
 
 
-def test_create_dhcp_relay_profile_when_api_should_return_successfully_created_message(
+def test_create_dhcp_relay_profile_should_return_api_response(
     mock_vmc_request_call_api,
 ):
     expected_response = {"message": "DHCP relay profile created successfully"}
@@ -185,7 +185,7 @@ def test_create_dhcp_relay_profile_when_api_should_return_successfully_created_m
     )
 
 
-def test_create_dhcp_server_profile_when_api_should_return_successfully_created_message(
+def test_create_dhcp_server_profile_should_return_api_response(
     mock_vmc_request_call_api,
 ):
     expected_response = {"message": "DHCP server profile created successfully"}
@@ -224,3 +224,83 @@ def test_create_dhcp_profile_called_with_url():
     call_kwargs = vmc_call_api.mock_calls[0][-1]
     assert call_kwargs["url"] == expected_url
     assert call_kwargs["method"] == vmc_constants.PUT_REQUEST_METHOD
+
+
+def test_update_dhcp_relay_profile_should_return_api_response(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"message": "DHCP relay profile updated successfully"}
+    mock_vmc_request_call_api.return_value = expected_response
+    assert (
+        vmc_dhcp_profiles.update(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="relay",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_update_dhcp_server_profile_should_return_api_response(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"message": "DHCP server profile updated successfully"}
+    mock_vmc_request_call_api.return_value = expected_response
+    assert (
+        vmc_dhcp_profiles.update(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="server",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_update_dhcp_profile_when_api_returns_no_dhcp_profile_with_given_id(
+    mock_vmc_request_call_api,
+):
+    expected_response = {"error": "Given DHCP profile does not exist"}
+    mock_vmc_request_call_api.return_value = expected_response
+    assert (
+        vmc_dhcp_profiles.update(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="relay",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+        == expected_response
+    )
+
+
+def test_update_dhcp_profile_called_with_url():
+    expected_url = (
+        "https://hostname/vmc/reverse-proxy/api/orgs/org_id/sddcs/sddc_id/policy/api/"
+        "v1/infra/dhcp-relay-configs/dhcp_profile_id"
+    )
+    with patch("saltext.vmware.utils.vmc_request.call_api", autospec=True) as vmc_call_api:
+        vmc_dhcp_profiles.update(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            type="relay",
+            dhcp_profile_id="dhcp_profile_id",
+            verify_ssl=False,
+        )
+    call_kwargs = vmc_call_api.mock_calls[0][-1]
+    assert call_kwargs["url"] == expected_url
