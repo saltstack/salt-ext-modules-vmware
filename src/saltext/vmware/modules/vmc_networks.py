37fcf584b453addc536f62fde3b9d2a0fc7bfa38
shaikre <shaikre@vmware.com>
2021-08-12 14:50:46 +0530

    Added delete function and corresponding tests for vmc_networks module

--- src/saltext/vmware/modules/vmc_networks.py
+++ src/saltext/vmware/modules/vmc_networks.py
@@ -169,3 +169,72 @@ def get_by_id(
         verify_ssl=verify_ssl,
         cert=cert,
     )
+
+
+def delete(
+    hostname,
+    refresh_key,
+    authorization_host,
+    org_id,
+    sddc_id,
+    network_id,
+    verify_ssl=True,
+    cert=None,
+):
+    """
+    Deletes given network/segment from the given SDDC
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt vm_minion vmc_networks.delete hostname=nsxt-manager.local network_id=web-tier ...
+
+    hostname
+        The host name of NSX-T manager
+
+    refresh_key
+        API Token of the user which is used to get the Access Token required for VMC operations
+
+    authorization_host
+        Hostname of the VMC cloud console
+
+    org_id
+        The Id of organization to which the SDDC belongs to
+
+    sddc_id
+        The Id of SDDC from which the network/segment should be deleted
+
+    network_id
+        Id of the network/segment to be deleted from SDDC
+
+    verify_ssl
+        (Optional) Option to enable/disable SSL verification. Enabled by default.
+        If set to False, the certificate validation is skipped.
+
+    cert
+        (Optional) Path to the SSL client certificate file to connect to VMC Cloud Console.
+        The certificate can be retrieved from browser.
+
+    """
+
+    log.info("Deleting Network %s for SDDC %s", network_id, sddc_id)
+    api_url_base = vmc_request.set_base_url(hostname)
+    api_url = (
+        "{base_url}vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/tier-1s/cgw/segments/{network_id}"
+    )
+    api_url = api_url.format(
+        base_url=api_url_base, org_id=org_id, sddc_id=sddc_id, network_id=network_id
+    )
+
+    return vmc_request.call_api(
+        method=vmc_constants.DELETE_REQUEST_METHOD,
+        url=api_url,
+        refresh_key=refresh_key,
+        authorization_host=authorization_host,
+        description="vmc_networks.delete",
+        responsebody_applicable=False,
+        verify_ssl=verify_ssl,
+        cert=cert,
+    )
