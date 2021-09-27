2e3bb2a23f8a998a429bcee08eb371b62150df71
cheburakshu <rajagopalasu@vmware.com>
2021-09-27 10:49:56 -0600

    address Wayne's review comments

--- tests/integration/modules/test_dvswitch.py
+++ tests/integration/modules/test_dvswitch.py
@@ -3,6 +3,8 @@
 import uuid
 
 import pytest
+import salt.exceptions
+import salt.ext.tornado.test.web_test
 import saltext.vmware.modules.dvswitch as dvswitch
 
 
@@ -14,7 +16,19 @@ def test_manage(service_instance):
     ret = dvswitch.manage(
         switch_name=switch_name,
         datacenter_name="Datacenter",
+        uplink_count=5,
+        uplink_prefix="Test ",
+        switch_version="6.5.0",
+        switch_description="New switch",
         mtu=2000,
+        discovery_protocol="cdp",
+        discovery_operation="listen",
+        multicast_filtering_mode="basic",
+        contact_name="Fred",
+        contact_description="Hello Fred",
+        network_forged_transmits=False,
+        network_mac_changes=False,
+        network_promiscuous=False,
         service_instance=service_instance,
     )
     assert ret
@@ -22,7 +36,40 @@ def test_manage(service_instance):
     ret = dvswitch.manage(
         switch_name=switch_name,
         datacenter_name="Datacenter",
+        uplink_count=8,
+        uplink_prefix="Test ",
+        switch_version="7.0.0",
+        switch_description="Updated switch",
         mtu=1600,
+        discovery_protocol="lldp",
+        discovery_operation="both",
+        multicast_filtering_mode="snooping",
+        contact_name="Tom",
+        contact_description="Hello Tom",
+        network_forged_transmits=True,
+        network_mac_changes=True,
+        network_promiscuous=True,
         service_instance=service_instance,
     )
+    assert ret
+
+    with pytest.raises(salt.exceptions.SaltException) as exc:
+        ret = dvswitch.manage(
+            switch_name=switch_name,
+            datacenter_name="Datacenter",
+            uplink_count=8,
+            uplink_prefix="Test ",
+            switch_version="7.0.0",
+            switch_description="Updated switch",
+            mtu=1600,
+            discovery_protocol="sdnp",
+            discovery_operation="both",
+            multicast_filtering_mode="snooping",
+            contact_name="Tom",
+            contact_description="Hello Tom",
+            network_forged_transmits=True,
+            network_mac_changes=True,
+            network_promiscuous=True,
+            service_instance=service_instance,
+        )
     assert ret
