0ce7ce35602c23ed6daa9cc3b9f2772beecc3368
Prerna <prjain@vmware.com>
2021-10-06 18:48:21 +0530

    function to get vCenter details of a SDDC

--- tests/unit/modules/test_vmc_sddc.py
+++ tests/unit/modules/test_vmc_sddc.py
@@ -289,3 +289,32 @@ def test_assert_sddc_update_should_correctly_filter_args(actual_args, expected_p
 
     call_kwargs = vmc_call_api.mock_calls[0][-1]
     assert call_kwargs["data"] == expected_payload
+
+
+def test_get_vcenter_detail_should_return_api_response(sddc_data_by_id, vcenter_data):
+    result = vmc_sddc.get_vcenter_detail(
+        hostname="hostname",
+        refresh_key="refresh_key",
+        authorization_host="authorization_host",
+        org_id="org_id",
+        sddc_id="sddc_id",
+        verify_ssl=False,
+    )
+
+    assert result == vcenter_data
+
+
+def test_get_vcenter_detail_called_with_url():
+    expected_url = "https://hostname/vmc/api/orgs/org_id/sddcs/sddc_id"
+    with patch("saltext.vmware.utils.vmc_request.call_api", autospec=True) as vmc_call_api:
+        vmc_sddc.get_vcenter_detail(
+            hostname="hostname",
+            refresh_key="refresh_key",
+            authorization_host="authorization_host",
+            org_id="org_id",
+            sddc_id="sddc_id",
+            verify_ssl=False,
+        )
+    call_kwargs = vmc_call_api.mock_calls[0][-1]
+    assert call_kwargs["url"] == expected_url
+    assert call_kwargs["method"] == vmc_constants.GET_REQUEST_METHOD
