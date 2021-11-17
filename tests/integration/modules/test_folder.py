31fca36d75c22d2b202bf0ba228d0e9f08fb713c
Joey Gibson <jgibson@saltstack.com>
2021-11-16 18:04:17 -0800

    manage folder changes

---
+++ tests/integration/modules/test_folder.py
@@ -0,0 +1,41 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import os
+import tarfile
+
+import pytest
+import salt.exceptions
+import saltext.vmware.modules.folder as folder
+
+
+def test_create(patch_salt_globals_folder):
+    """
+    Test folder create
+    """
+    ret = folder.create('test_folder', 'Datacenter', 'vm')
+    assert ret["status"] == "created"
+
+
+def test_rename(patch_salt_globals_folder):
+    """
+    Test folder rename
+    """
+    ret = folder.rename('test_folder', 'new_test_folder', 'Datacenter', 'vm')
+    assert ret["status"] == "renamed"
+
+
+def test_move(patch_salt_globals_folder):
+    """
+    Test folder move
+    """
+    ret = folder.create('test_folder', 'Datacenter', 'vm')
+    ret = folder.move('test_folder', 'new_test_folder', 'Datacenter', 'vm')
+    assert ret["status"] == "moved"
+
+
+def test_destoryed(patch_salt_globals_folder):
+    """
+    Test folder destoryed
+    """
+    ret = folder.destroy('new_test_folder', 'Datacenter', 'vm')
+    assert ret["status"] == "destroyed"
--- tests/integration/states/test_vm.py
+++ tests/integration/states/test_vm.py
@@ -1,7 +1,5 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
-import uuid
-
 import pytest
 import saltext.vmware.states.vm as virtual_machine
 
