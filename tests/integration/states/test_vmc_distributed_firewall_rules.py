96f94d5aa93317d70a259ccbd77dcf17a318a828
Nava Patel <pnaval@vmware.com>
2022-03-01 15:57:20 +0530

    Refactored code state it tests

--- tests/integration/states/test_vmc_distributed_firewall_rules.py
+++ tests/integration/states/test_vmc_distributed_firewall_rules.py
@@ -5,7 +5,97 @@ import pytest
 import requests
 from saltext.vmware.utils import vmc_request
 
-from tests.integration.conftest import get_config
+
+@pytest.fixture
+def request_headers(common_data):
+    return vmc_request.get_headers(common_data["refresh_key"], common_data["authorization_host"])
+
+
+@pytest.fixture
+def distributed_firewall_rule_url(common_data):
+    url = (
+        "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/domains/{domain_id}/security-policies/{security_policy_id}/rules/{rule_id}"
+    )
+    api_url = url.format(
+        hostname=common_data["hostname"],
+        org_id=common_data["org_id"],
+        sddc_id=common_data["sddc_id"],
+        domain_id=common_data["domain_id"],
+        security_policy_id=common_data["security_policy_id"],
+        rule_id=common_data["rule_id"],
+    )
+    return api_url
+
+
+@pytest.fixture
+def distributed_firewall_rules_list_url(common_data):
+    url = (
+        "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/domains/{domain_id}/security-policies/{security_policy_id}/rules"
+    )
+    api_url = url.format(
+        hostname=common_data["hostname"],
+        org_id=common_data["org_id"],
+        sddc_id=common_data["sddc_id"],
+        domain_id=common_data["domain_id"],
+        security_policy_id=common_data["security_policy_id"],
+    )
+    return api_url
+
+
+@pytest.fixture
+def common_data(vmc_nsx_connect):
+    hostname, refresh_key, authorization_host, org_id, sddc_id, verify_ssl, cert = vmc_nsx_connect
+    data = {
+        "hostname": hostname,
+        "refresh_key": refresh_key,
+        "authorization_host": authorization_host,
+        "org_id": org_id,
+        "sddc_id": sddc_id,
+        "domain_id": "default",
+        "security_policy_id": "default-layer3-section",
+        "rule_id": "Integration_state_DFR_1",
+        "verify_ssl": verify_ssl,
+        "cert": cert,
+    }
+    yield data
+
+
+@pytest.fixture
+def get_distributed_firewall_rules(
+    common_data, distributed_firewall_rules_list_url, request_headers
+):
+    session = requests.Session()
+    response = session.get(
+        url=distributed_firewall_rules_list_url,
+        verify=common_data["cert"] if common_data["verify_ssl"] else False,
+        headers=request_headers,
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+@pytest.fixture
+def delete_distributed_firewall_rule(
+    get_distributed_firewall_rules, distributed_firewall_rule_url, request_headers, common_data
+):
+    """
+    Sets up test requirements:
+    Queries vmc api for distributed firewall rules
+    Deletes distributed firewall rule if exists
+    """
+
+    for result in get_distributed_firewall_rules.get("results", []):
+        if result["id"] == common_data["rule_id"]:
+            session = requests.Session()
+            response = session.delete(
+                url=distributed_firewall_rule_url,
+                verify=common_data["cert"] if common_data["verify_ssl"] else False,
+                headers=request_headers,
+            )
+            # raise error if any
+            response.raise_for_status()
 
 
 @pytest.fixture()
@@ -17,85 +107,6 @@ def distributed_firewall_rule_test_data():
     updated_display_name = "rule-1"
     updated_tags = [{"tag": "tag1", "scope": "scope1"}]
     return domain_id, security_policy_id, rule_id, display_name, updated_display_name, updated_tags
-
-
-@pytest.fixture
-def get_distributed_firewall_rules(vmc_nsx_connect, distributed_firewall_rule_test_data):
-    hostname, refresh_key, authorization_host, org_id, sddc_id, verify_ssl, cert = vmc_nsx_connect
-    (
-        domain_id,
-        security_policy_id,
-        rule_id,
-        display_name,
-        updated_display_name,
-        updated_tags,
-    ) = distributed_firewall_rule_test_data
-    url = (
-        "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
-        "policy/api/v1/infra/domains/{domain_id}/security-policies/{security_policy_id}/rules"
-    )
-    api_url = url.format(
-        hostname=hostname,
-        org_id=org_id,
-        sddc_id=sddc_id,
-        domain_id=domain_id,
-        security_policy_id=security_policy_id,
-    )
-
-    session = requests.Session()
-    response = session.get(
-        url=api_url,
-        verify=cert if verify_ssl else False,
-        headers=vmc_request.get_headers(refresh_key, authorization_host),
-    )
-    response.raise_for_status()
-    return response.json()
-
-
-@pytest.fixture
-def delete_distributed_firewall_rule(
-    get_distributed_firewall_rules, vmc_nsx_connect, distributed_firewall_rule_test_data
-):
-    """
-    Sets up test requirements:
-    Queries vmc api for distributed firewall rules
-    Deletes distributed firewall rule if exists
-    """
-    hostname, refresh_key, authorization_host, org_id, sddc_id, verify_ssl, cert = vmc_nsx_connect
-    (
-        domain_id,
-        security_policy_id,
-        rule_id,
-        display_name,
-        updated_display_name,
-        updated_tags,
-    ) = distributed_firewall_rule_test_data
-
-    distributed_firewall_rules_dict = get_distributed_firewall_rules
-    if distributed_firewall_rules_dict["result_count"] != 0:
-        results = distributed_firewall_rules_dict["results"]
-        for result in results:
-            if result["id"] == rule_id:
-                url = (
-                    "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
-                    "policy/api/v1/infra/domains/{domain_id}/security-policies/{security_policy_id}/rules/{rule_id}"
-                )
-                api_url = url.format(
-                    hostname=hostname,
-                    org_id=org_id,
-                    sddc_id=sddc_id,
-                    domain_id=domain_id,
-                    security_policy_id=security_policy_id,
-                    rule_id=rule_id,
-                )
-                session = requests.Session()
-                response = session.delete(
-                    url=api_url,
-                    verify=cert if verify_ssl else False,
-                    headers=vmc_request.get_headers(refresh_key, authorization_host),
-                )
-                # raise error if any
-                response.raise_for_status()
 
 
 def test_vmc_distributed_firewall_rules_state_module(
