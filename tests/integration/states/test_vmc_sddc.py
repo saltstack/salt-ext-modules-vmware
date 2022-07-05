65da4154010dd35839a0065364f4ef586c80117c
shaikre <shaikre@vmware.com>
2022-07-05 11:27:43 +0530

    Fixed IT testcases

--- tests/integration/states/test_vmc_sddc.py
+++ tests/integration/states/test_vmc_sddc.py
@@ -17,14 +17,17 @@ def vmc_common_data(vmc_connect):
 
 
 @pytest.fixture
-def request_headers(common_data):
-    return vmc_request.get_headers(common_data["refresh_key"], common_data["authorization_host"])
+def request_headers(vmc_common_data):
+    return vmc_request.get_headers(
+        vmc_common_data["refresh_key"], vmc_common_data["authorization_host"]
+    )
 
 
 @pytest.fixture
 def sddc_name():
-    now = datetime.now()
-    return "sddc_test-" + now
+    # now = datetime.now()
+    # return "sddc_test-" + str(now)
+    return "123-salt-sddc-it-test-2022"
 
 
 @pytest.fixture
@@ -63,7 +66,7 @@ def delete_sddc(
     Deletes SDDCs if exists with the same sddc_name
     """
 
-    for result in get_sddc_list.get("results", []):
+    for result in get_sddc_list:
         if result["name"] == sddc_name:
             session = requests.Session()
             response = session.delete(
@@ -75,29 +78,15 @@ def delete_sddc(
             response.raise_for_status()
 
 
-def get_sddc(vmc_common_data, request_headers, sddc_id):
-    url = "https://{hostname}/vmc/api/orgs/{org_id}/sddcs/{sddc_id}"
-    api_url = url.format(
-        hostname=vmc_common_data["hostname"],
-        org_id=vmc_common_data["org_id"],
-        sddc_id=sddc_id,
-    )
-    session = requests.Session()
-    response = session.get(
-        url=api_url,
-        verify=vmc_common_data["cert"] if vmc_common_data["verify_ssl"] else False,
-        headers=request_headers,
-    )
-    response.raise_for_status()
-    return response.json()
-
-
 def test_vmc_sddc_state_module(salt_call_cli, vmc_common_data, delete_sddc, sddc_name):
     # Invoke present state to create sddc
     response = salt_call_cli.run(
         "state.single",
         "vmc_sddc.present",
         name=sddc_name,
+        num_hosts=1,
+        provider="ZEROCLOUD",
+        region="us-west-2",
         **vmc_common_data,
     )
     response_json = response.json
@@ -105,25 +94,35 @@ def test_vmc_sddc_state_module(salt_call_cli, vmc_common_data, delete_sddc, sddc
     changes = result["changes"]
 
     assert changes["old"] is None
-    assert changes["new"]["name"] == sddc_name
+    assert changes["new"]["params"]["sddcConfig"]["name"] == sddc_name
     assert result["comment"] == "Created SDDC {}".format(sddc_name)
 
-    sddc_id = result["changes"]["new"]["resource_id"]  # get the sddc_id of newly created SDDC
+    # get the sddc_id of newly created SDDC
+    sddc_id = result["changes"]["new"]["resource_id"]
 
     # Invoke present state where sddc already exist
     response = salt_call_cli.run(
         "state.single",
         "vmc_sddc.present",
         name=sddc_name,
+        num_hosts=1,
+        provider="ZEROCLOUD",
+        region="us-west-2",
         **vmc_common_data,
     )
     response_json = response.json
     result = list(response_json.values())[0]
     changes = result["changes"]
 
-    assert changes["old"] is None
-    assert changes["new"] is None
+    # assert no changes are done
+    assert changes == {}
     assert result["comment"] == "SDDC is already present"
+
+    # Get the SDDC data
+    ret = salt_call_cli.run("vmc_sddc.get_by_id", sddc_id=sddc_id, **vmc_common_data)
+    result_as_json = ret.json
+    assert "error" not in result_as_json
+    sddc = result_as_json
 
     # Invoke absent to delete the sddc
     response = salt_call_cli.run("state.single", "vmc_sddc.absent", name=sddc_id, **vmc_common_data)
@@ -131,7 +130,6 @@ def test_vmc_sddc_state_module(salt_call_cli, vmc_common_data, delete_sddc, sddc
     result = list(response_json.values())[0]
     changes = result["changes"]
 
-    sddc = get_sddc(vmc_common_data, request_headers, sddc_id)
     if sddc.get("sddc_state") in ("DEPLOYING", "DELETED", "DELETION_IN_PROGRESS"):
         assert changes == {}
         assert result[
