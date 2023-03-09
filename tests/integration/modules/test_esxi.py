d05f1b77e52109e0e27fcde6e1491419a8af125c
Joey Gibson <gjoey@vmware.com>
2023-03-09 11:40:16 -0800

    get service policy

--- tests/integration/modules/test_esxi.py
+++ tests/integration/modules/test_esxi.py
@@ -846,3 +846,15 @@ def test_list_diskgroups(service_instance):
         service_instance=service_instance,
     )
     assert ret
+
+
+def test_get_service_policy(service_instance):
+    ret = esxi.get_service_policy(
+        "ssh",
+        service_instance=service_instance,
+    )
+    assert ret
+    for host_name in ret:
+        for service in ret[host_name]:
+            assert service == "ssh"
+            assert isinstance(ret[host_name][service], str)
