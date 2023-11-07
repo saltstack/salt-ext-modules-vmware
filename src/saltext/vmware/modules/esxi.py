97bedfaa2e18c42ea3bffee72beda9f63cb10eda
Jruta12 <144878374+Jruta12@users.noreply.github.com>
2023-11-07 17:22:05 +0530

    Update esxi.py

--- src/saltext/vmware/modules/esxi.py
+++ src/saltext/vmware/modules/esxi.py
@@ -4157,6 +4157,16 @@ def get_reference_schema():
     log.debug("Running vmware_esxi.retrieve_reference_schema")
     return retrieve_reference_schema(Product.ESX)
 
+def convert_ordered_dict_to_dict(obj):
+    if isinstance(obj, dict):
+        return {key: convert_ordered_dict_to_dict(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_ordered_dict_to_dict(element) for element in obj]
+    elif isinstance(obj, OrderedDict):
+        return convert_ordered_dict_to_dict(dict(obj))
+    else:
+        return obj
+
 def check_compliance(profile=None, cluster_paths=None, desired_state_spec=None, esx_config=None):
     """
     Checks compliance of cluster.
@@ -4179,13 +4189,21 @@ def check_compliance(profile=None, cluster_paths=None, desired_state_spec=None, 
     """
     
     log.info("Checking complaince %s", desired_state_spec)
+    desired_state_spec = convert_ordered_dict_to_dict(desired_state_spec)    
     config = __opts__
-    if desired_state_spec == None:
-        desired_state_spec = json.loads(json.dumps(desired_state_spec))
     if not esx_config:
         esx_config = utils_esxi.create_esx_config(config, profile)
-    response_check_compliance = esx_config.check_compliance(desired_state_spec= desired_state_spec, cluster_paths= cluster_paths)
-    return response_check_compliance
+  
+	try:
+        response_check_compliance = esx_config.check_compliance(
+            desired_state_spec= desired_state_spec, cluster_paths= cluster_paths
+        )
+        return {"details": response_check_compliance}
+
+	except Exception as e:
+        #  including exception details
+        log.error("Check Compliance encountered an error: %s", str(e))
+        return {"status": False, "details": str(e)}
 
 
 
