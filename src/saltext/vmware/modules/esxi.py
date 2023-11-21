c0a9f5a0ffdabdc593391b2c3a38b1aca449569e
jruta <jruta@vmware.com>
2023-11-21 12:15:13 +0530

    Removing the duplicate code

--- src/saltext/vmware/modules/esxi.py
+++ src/saltext/vmware/modules/esxi.py
@@ -4336,39 +4336,6 @@ def get_reference_schema():
     return retrieve_reference_schema(Product.ESX)
 
 
-def check_compliance(profile=None, cluster_paths=None, desired_state_spec=None, esx_config=None):
-    """
-    Checks compliance of cluster.
-
-    desired_state_spec
-        Gets the desired spec from sls file
-
-    cluster_paths
-        Gets the configuration from the cluster paths specified
-
-    esx_config
-        If there is an esx_config instance already available it can be provided, otherwise a new one will be created. (optional)
-
-    profile
-        Profile to use (optional)
-
-    .. code-block:: bash
-
-        salt-call vmware_esxi.check_compliance cluster_paths="SDDC-Datacenter/vlcm_cluster1"
-    """
-    config = __opts__
-    if not esx_config:
-        esx_config = utils_esxi.create_esx_config(config, profile)
-    try:
-        response_check_compliance = esx_config.check_compliance(
-            desired_state_spec=desired_state_spec, cluster_paths=cluster_paths
-        )
-        return {"details": response_check_compliance}
-    except Exception as e:
-        log.error("Check Compliance encountered an error: %s", str(e))
-        return {"details": str(e)}
-
-
 def calculate_precheck_status(data):
     """
     Calculate the precheck status dynamically based on the presence of "OK" in the precheck response.
