0655c54e48443d996a611ca90da9c5ed51dbb5d6
Raja Murugesan <rajagopal.it@gmail.com>
2023-12-14 09:52:27 -0600

    VCFSC-42: Add salt state and execution modules for jpmc compliance check PoC. (#396)
    * VCFSC-42: Add salt state and execution modules for jpmc compliance check use cases.
    
    * Add vcenter state module
    
    * rename module names.
    
    * incorporate new config module changes.
    
    ---------
    
    Co-authored-by: ramurugesan <ramurugesan@vmware.com>

---
+++ src/saltext/vmware/modules/compliance_control.py
@@ -0,0 +1,80 @@
+# SPDX-License: Apache-2.0
+import logging
+
+import salt.exceptions
+import saltext.vmware.utils.vc as utils_vc
+
+log = logging.getLogger(__name__)
+
+__virtualname__ = "vmware_compliance_control"
+
+
+def __virtual__():
+    return __virtualname__
+
+
+def control_config_compliance_check(control_config, product, vc_config=None, profile=None):
+    """
+    Checks compliance of vcenter control config. Control config can be ntp, dns, syslog, etc.
+    Returns control compliance response object.
+
+    control_config
+        vc control config dict object.
+    product
+        appliance name. vcenter, nsx, etc.
+    vc_config
+        Optional vc_config object with vc connection.
+
+    profile
+        Optional auth profile to be used for vc connection.
+    """
+
+    log.info("Checking complaince %s", control_config)
+    if not vc_config:
+        config = __opts__
+        vc_config = utils_vc.create_vc_config(config, profile)
+
+    try:
+        response_check_compliance = vc_config.check_compliance(
+            product=product, desired_state_spec=control_config
+        )
+        log.debug("control_config_compliance_check response %s", response_check_compliance)
+        return response_check_compliance
+    except Exception as exc:
+        log.error("control_config_compliance_check encountered an error: %s", str(exc))
+        raise salt.exceptions.VMwareRuntimeError(str(exc))
+
+
+def control_config_remediate(control_config, product, vc_config=None, profile=None):
+    """
+    Remediate given compliance control config. Control config can be ntp, dns, syslog, etc.
+    Returns remediation response object.
+
+    control_config
+        vc control config dict object.
+    product
+        appliance name. vcenter, nsx, etc.
+    vc_config
+        Optional vc_config object with vc connection.
+
+    profile
+        Optional auth profile to be used for vc connection.
+    """
+
+    log.info("control_config_remediate: %s", control_config)
+
+    if not vc_config:
+        config = __opts__
+        vc_config = utils_vc.create_vc_config(config, profile)
+
+    try:
+        response_remediate = vc_config.remediate_with_desired_state(
+            product=product, desired_state_spec=control_config
+        )
+        log.debug("control_config_remediate response %s", response_remediate)
+        return response_remediate
+
+    except Exception as exc:
+        # Handle exceptions by setting status as false and including exception details
+        log.error("control_config_remediate encountered an error: %s", str(exc))
+        raise salt.exceptions.VMwareRuntimeError(str(exc))
