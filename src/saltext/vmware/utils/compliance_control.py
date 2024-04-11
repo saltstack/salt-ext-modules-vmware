109592b6afbaef008a5bef0d1529754d5aab37ce
ramurugesan <ramurugesan@vmware.com>
2024-04-11 15:12:32 -0500

    Retrieve hostname from grains and fallback to pillar

--- src/saltext/vmware/utils/compliance_control.py
+++ src/saltext/vmware/utils/compliance_control.py
@@ -22,27 +22,27 @@ except ImportError:
 log = logging.getLogger(__name__)
 
 
-def _create_vcenter_context(conf):
+def _create_vcenter_context(conf, fqdn):
     return VcenterContext(
-        hostname=conf["host"],
+        hostname=fqdn,
         username=conf["user"],
         password=conf["password"],
         ssl_thumbprint=conf.get("ssl_thumbprint", None),
     )
 
 
-def _create_sddc_manager_context(conf):
+def _create_sddc_manager_context(conf, fqdn):
     return SDDCManagerContext(
-        hostname=conf["host"],
+        hostname=fqdn,
         username=conf["user"],
         password=conf["password"],
         ssl_thumbprint=conf.get("ssl_thumbprint", None),
     )
 
 
-def _create_esxi_context(vcenter_conf, ids=None):
+def _create_esxi_context(vcenter_conf, fqdn=None, ids=None):
     return EsxiContext(
-        vc_hostname=vcenter_conf["host"],
+        vc_hostname=fqdn,
         vc_username=vcenter_conf["user"],
         vc_password=vcenter_conf["password"],
         vc_ssl_thumbprint=vcenter_conf.get("ssl_thumbprint", None),
@@ -66,8 +66,8 @@ def _create_product_context(config, product, ids=None):
             or config.get("grains", {}).get("saltext.vmware")
             or config.get("pillar", {}).get("saltext.vmware")
             or config.get(parent_product)
-            or config.get("grains", {}).get(parent_product)
             or config.get("pillar", {}).get(parent_product)
+            or config.get("grains", {}).get(parent_product)
             or {}
         )
     else:
@@ -76,23 +76,25 @@ def _create_product_context(config, product, ids=None):
             or config.get("grains", {}).get("saltext.vmware")
             or config.get("pillar", {}).get("saltext.vmware")
             or config.get(product)
-            or config.get("grains", {}).get(product)
             or config.get("pillar", {}).get(product)
+            or config.get("grains", {}).get(product)
             or {}
         )
-
+    fqdn = config.get("grains", {}).get("fqdn")
+    if not fqdn:
+        fqdn = conf.get("host")
     if product == BaseContext.ProductEnum.VCENTER.value:
-        return _create_vcenter_context(conf)
+        return _create_vcenter_context(conf, fqdn)
     elif product == BaseContext.ProductEnum.SDDC_MANAGER.value:
-        return _create_sddc_manager_context(conf)
+        return _create_sddc_manager_context(conf, fqdn)
     elif product == BaseContext.ProductEnum.NSXT_MANAGER.value:
         return BaseContext(BaseContext.ProductEnum.NSXT_MANAGER)
     elif product == BaseContext.ProductEnum.NSXT_EDGE.value:
         return BaseContext(BaseContext.ProductEnum.NSXT_EDGE)
     elif product == BaseContext.ProductEnum.ESXI.value:
-        return _create_esxi_context(vcenter_conf=conf, ids=ids)
+        return _create_esxi_context(vcenter_conf=conf, fqdn=fqdn, ids=ids)
     elif product == BaseContext.ProductEnum.VRSLCM.value:
-        return VrslcmContext(conf["host"])
+        return VrslcmContext(fqdn)
     else:
         raise salt.exceptions.VMwareApiError({f"Unsupported product {product}"})
 
