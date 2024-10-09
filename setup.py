445949ae0b0f24985241e197b85879aaaf1bf14f
David Murphy <dmurphy@saltstack.com>
2024-10-09 09:57:11 -0600

    Trying to obtain version based off tag (#412)
    * Trying to obtain version based off tag
    
    * Removed the version.py file since this is now generated

--- setup.py
+++ setup.py
@@ -3,17 +3,5 @@ from pathlib import Path
 
 import setuptools
 
-version_path = Path(__file__).parent / "src" / "saltext" / "vmware" / "version.py"
-
-with version_path.open() as f:
-    for line in f:
-        if line.startswith("__version__"):
-            # We only want the bare string pls
-            version = line.partition("=")[-1].strip().strip('"').strip("'")
-            break
-    else:
-        version = "0.0.1dev1"
-
-
 if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True, version=version)
+    setuptools.setup(use_scm_version=True)
